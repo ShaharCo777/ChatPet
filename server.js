d9eef74a7be981d198cd4d8afc007d3151769f9d
ShaharCo777 <57673437+ShaharCo777@users.noreply.github.com>
2020-02-18 12:34:42 +0200

    git@github.com:ShaharCo777/ChatPet.git
    all th project

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express   = require('express'),
       connectDB = require("./config/db"),
-      cors = require('cors');
+      path = require('path'),
+      cors = require('cors')
 
 
 
@@ -22,6 +23,17 @@ app.use('/api/pets', require('./Routes/api/pets'))
 app.use('/api/profile', require('./Routes/api/profile'))
 
   
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client-side/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client-side', 'build', 'index.html'));
+  })
+}
+
+
 const PORT = process.env.PORT || 5000
 
 
